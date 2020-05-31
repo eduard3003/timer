5929584242756ce4257b7ba40afca5cbfc64c774
Eduard <61876738+eduard3003@users.noreply.github.com>
2020-05-31 13:42:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,6 @@ window.addEventListener('DOMContentLoaded', function () {
 
     'use strict'; //Переводим весь наш код в строгий режим
 
-    // Табы:
-    let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
-
-    function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-        }
-    }
-    hideTabContent(1);
-
-    function showtabContent(b) {
-        if (tabContent[b].classList.contains('hide')) {
-            tabContent[b].classList.remove('hide');
-            tabContent[b].classList.add('show');
-        }
-    }
-    info.addEventListener('click', function (event) {
-        let target = event.target;
-        if (target && target.classList.contains('info-header-tab')) {
-            for (let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
-                    hideTabContent(0);
-                    showtabContent(i);
-                    break; //останавливаем цикл
-                }
-            }
-        }
-    });
-
     // Timer:
 
     let deadline = '2020-06-02'; // задаем конечную дату
